6a5cefb9fe2d32d0c8dc9baf9960c8c8bc270900
Iaroslav Derkach <141680960+JaDerkach@users.noreply.github.com>
2023-08-18 00:56:08 +0300

    add to task 1,2,3

---
+++ hw2.js
@@ -0,0 +1,32 @@
+let arr=[
+    100, "name", "book","page","username", 323.14, -0.33, 42100, `okten`, `Lesson`
+]
+console.log(arr[0])
+console.log(arr[1])
+console.log(arr[2])
+console.log(arr[3])
+console.log(arr[4])
+console.log(arr[5])
+console.log(arr[6])
+console.log(arr[7])
+console.log(arr[8])
+console.log(arr[9])
+
+// ================================================================================
+
+let descriptionBooks =[
+    ["title"],
+    ["pageCount"],
+    ["genre"],
+]
+console.log(descriptionBooks)
+
+// -------------------------------------------------------------------------------
+
+let description =[
+    ["title"],
+    ["pageCount"],
+    ["genre"],
+    ["authors", [`name`, `age`]]
+]
+console.log(description)
